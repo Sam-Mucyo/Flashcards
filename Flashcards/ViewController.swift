f80557d5c226a8b32069e832ad3031dba7d8d568
Sam <sammucyo@college.harvard.edu>
2022-03-04 21:21:40 -0500

    Completed the basics of Flashcard app

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var frontLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tapFlashcard = UITapGestureRecognizer(target: self, action: #selector(didTapFlashcard(_:)) )
+        tapFlashcard.numberOfTapsRequired = 1
+        tapFlashcard.numberOfTouchesRequired = 1
+        frontLabel.addGestureRecognizer(tapFlashcard)
+        frontLabel.isUserInteractionEnabled = true
+        
     }
-
-
+    @IBAction func didTapFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
 }
 
