9fbd76dc374bbf938812c543c8da853dfdab9329
Sam <sammucyo@college.harvard.edu>
2022-03-19 01:06:42 -0400

    Lab 2 Completed.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -16,13 +16,36 @@ class ViewController: UIViewController {
         let tapFlashcard = UITapGestureRecognizer(target: self, action: #selector(didTapFlashcard(_:)) )
         tapFlashcard.numberOfTapsRequired = 1
         tapFlashcard.numberOfTouchesRequired = 1
+        
         frontLabel.addGestureRecognizer(tapFlashcard)
+        backLabel.addGestureRecognizer(tapFlashcard)
+        backLabel.isUserInteractionEnabled = true
         frontLabel.isUserInteractionEnabled = true
         
     }
     @IBAction func didTapFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if frontLabel.isHidden {
+            frontLabel.isHidden = false
+        }
+        else {
+            frontLabel.isHidden = true
+        }
     }
     
+    func updateFlashcard(question: String, answer: String) {
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // We know the destination of the segue is the Navigation Controller
+    let navigationController = segue.destination as! UINavigationController
+    // We know the Navigation Controller only contains a Creation View Controller
+    let creationController = navigationController.topViewController as! CreationViewController
+        
+    // We set the flashcardsController property to self
+    creationController.flashcardsController = self
+    }
 }
 
